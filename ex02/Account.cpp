c0f34b5835cb2a25759c6e9c865af9d777e8395b
Pol Martínez Badenes <polmarti@car2s6.42barcelona.com>
2024-09-24 20:33:36 +0200

    in process

---
+++ ex02/Account.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: polmarti <polmarti@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 20:06:48 by polmarti          #+#    #+#             */
+/*   Updated: 2024/09/24 20:06:50 by polmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+static int	getNbAccounts( void )
+{}
+static int	getTotalAmount( void )
+{}
+static int	getNbDeposits( void )
+{};
+static int	getNbWithdrawals( void )
+{};
+static void	displayAccountsInfos( void )
+{};
+Account()
+{};
+Account( int initial_deposit )
+{};
+~Account( void )
+{};
+void	makeDeposit( int deposit )
+{};
+bool	makeWithdrawal( int withdrawal )
+{};
+int		checkAmount( void ) const
+{};
+void	displayStatus( void ) const
+{};
